c1377c8384199ad668e987684812de4e462271a2
Usama Rahman <rahmanusama390@gmail.com>
2023-10-13 22:13:02 -0800

    q

--- app.js
+++ app.js
@@ -26,4 +26,11 @@ const gineaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const deadlineformat = document.querySelectorAll(".deadline-format h4");
 
+let futureDate =  new Date(2023, 10, 29, 6, 30 ,0);
 
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+gineaway.textContent = `giveaway ends on ${year} ${hours}:${minutes}pm`;
+ 
